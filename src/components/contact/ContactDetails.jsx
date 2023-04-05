da7ff33ffd76f1a0239f7ceee2e746af2206b2d7
ramizqazi <ramizqazi03@gmail.com>
2023-04-05 12:57:33 +0500

    fix details

--- src/components/contact/ContactDetails.jsx
+++ src/components/contact/ContactDetails.jsx
@@ -4,17 +4,17 @@ import { FiPhone, FiMapPin, FiMail } from 'react-icons/fi';
 const contacts = [
 	{
 		id: 1,
-		name: 'Your Address, Your City, Your Country',
+		name: 'Karachi, Pakistan',
 		icon: <FiMapPin />,
 	},
 	{
 		id: 2,
-		name: 'email@domain.com',
+		name: 'ramizqazi03@gmail.com',
 		icon: <FiMail />,
 	},
 	{
 		id: 3,
-		name: '555 8888 888',
+		name: '0347 1258 140',
 		icon: <FiPhone />,
 	},
 ];
