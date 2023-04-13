f3cf9de327fe51f18e823e7f7671f936ee715911
Rameez Qazi <77078731+ramizqazi@users.noreply.github.com>
2023-04-14 03:20:02 +0500

    change bio

--- src/data/aboutMeData.js
+++ src/data/aboutMeData.js
@@ -1,14 +1,18 @@
 export const aboutMeData = [
 	{
 		id: 1,
-		bio: 'Hello there, I am Ramiz a 20 years-old Front-End Web and Mobile App developer with an extensive experience in Web and Mobile technologies. I am a keen observer and have a passion to put my best efforts into innovation and creativity. I’m confident that, with my skills and experience, I complete my job most effectively and collaboratively.',
+		bio: "Welcome to my portfolio! My name is Ramiz, and I'm a 20-year-old Front-End Web and Mobile App Developer with a passion for innovation and creativity. With years of experience in the field, I have developed expertise in various web and mobile technologies.",
 	},
 	{
 		id: 2,
-		bio: 'I have a fair amount of expertise in developing Front-End Apps with a varying range of technologies. I have used HTML5, CSS3, Javascript, and React.js to create meaningful front-end apps. I also make use of React Native for making cross-platform mobile apps that can run on Android and iOS and have a native feel and look. I ensure my client’s satisfaction and can easily adapt to different environments.',
+		bio: "I take pride in my ability to create stunning and meaningful front-end apps with HTML5, CSS3, Javascript, and React.js. Additionally, I am proficient in using React Native to develop cross-platform mobile apps that offer a native feel and look to users on both Android and iOS platforms.",
 	},
 	{
 		id: 3,
-		bio: 'Thanks for visiting my portfolio. If you want to your app/website click on hire me.',
+		bio: "Collaboration and client satisfaction are my top priorities. I always strive to complete my projects most effectively and efficiently, and I easily adapt to different environments. Whether you need a new app or a website, I'm confident that my skills and experience can make it happen.",
+	},
+	{
+		id: 4,
+		bio: "Thank you for visiting my portfolio, and I look forward to hearing from you soon. Click on Hire Me if you'd like to learn more about what I can do for your business.",
 	},
 ];
