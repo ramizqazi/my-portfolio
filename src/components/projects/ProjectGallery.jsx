6e0781135d9eae6d73b5c41e811abc1d6817a4e3
ramizqazi <ramizqazi03@gmail.com>
2023-05-26 04:01:46 +0500

    fix projects data

--- src/components/projects/ProjectGallery.jsx
+++ src/components/projects/ProjectGallery.jsx
@@ -1,38 +1,51 @@
 /* eslint-disable @next/next/no-img-element */
-"use client"
-import { useContext } from 'react';
+'use client';
+import {useContext, useState} from 'react';
+import ImageViewer from 'react-simple-image-viewer';
 import SingleProjectContext from '../../context/SingleProjectContext';
 
 import WebProjectImg from '../../images/web-project-2.jpg';
 import MobileProjectImg from '../../images/web-project-1.jpg';
 
 const ProjectGallery = () => {
-	const { singleProjectData } = useContext(SingleProjectContext);
+  const [selectedImg, setSelectedImg] = useState(null);
+  const {singleProjectData} = useContext(SingleProjectContext);
+  const images = [
+    ...singleProjectData.images.map(i => i.src),
+    singleProjectData?.category === 'Web Application'
+      ? WebProjectImg.src
+      : MobileProjectImg.src,
+  ];
 
-	return (
-		<div className="grid grid-cols-1 sm:grid-cols-3 sm:gap-10 mt-12">
-			{singleProjectData.images.map((img) => {
-				return (
-					<img
-						key={img.src}
-						src={img.src}
-						className="rounded-xl h-full cursor-pointer shadow-lg sm:shadow-none"
-						alt={img.src}
-					/>
-				);
-			})}
-			{singleProjectData?.category === 'Web Application' && <img
-							src={WebProjectImg.src}
-							className="rounded-xl h-full cursor-pointer shadow-lg sm:shadow-none"
-							alt='web project'
-						/>}
-			{singleProjectData?.category === 'Mobile Application' && <img
-							src={MobileProjectImg.src}
-							className="rounded-xl h-full cursor-pointer shadow-lg sm:shadow-none"
-							alt='web project'
-						/>}
-		</div>
-	);
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-3 sm:gap-10 mt-12">
+      {images.map((img, index) => {
+        return (
+          <img
+            key={img}
+            src={img}
+            className="rounded-xl h-full cursor-pointer shadow-lg sm:shadow-none"
+            alt={img}
+            onClick={() => {
+              setSelectedImg(index + 1);
+            }}
+          />
+        );
+      })}
+      {!!selectedImg && (
+        <ImageViewer
+          src={images}
+          currentIndex={selectedImg - 1}
+          onClose={() => setSelectedImg(null)}
+          disableScroll={false}
+          closeOnClickOutside={true}
+          backgroundStyle={{
+            backgroundColor: 'rgba(0,0,0,0.9)',
+          }}
+        />
+      )}
+    </div>
+  );
 };
 
 export default ProjectGallery;
--- src/data/projects.js
+++ src/data/projects.js
@@ -13,193 +13,195 @@ import Reap1 from '../images/reap-1.png';
 import Reap2 from '../images/reap-2.png';
 
 export const projectsData = [
-	{
-		id: 1,
-		title: 'Discreedly Health Platform',
-		category: 'Web Application',
-		images: [DiscreedlyImg1, DiscreedlyImg2],
-		ProjectHeader: {
-			title: 'Front-End Develpement',
-			publishDate: 'Jul 26, 2020',
-			tags: 'UI / Frontend',
-		},
-		details: {
-			projectDescription: 'The Discreedly app is designed to help HCPs and MSLs manage their daily tasks more efficiently. The app will provide a platform for HCPs and MSLs to communicate with each other, share information, and collaborate on projects. The app will also provide access to the latest medical research, clinical trials, and other relevant information.',			
-			features: [
-				'Secure messaging platform for HCPs and MSLs to communicate with each other',
-				'Access to the latest medical research, clinical trials, and other relevant information',
-				'Customizable dashboard that allows users to tailor the app to their specific needs',
-				'Task management system that helps users manage their daily tasks more efficiently'
-			]
-		},
-		technologies:
-			{
-				title: 'Tools & Technologies',
-				techs: [
-					'HTML',
-					'CSS',
-					'JavaScript',
-					'React.js',
-					'TailwindCSS',
-					'AdobeXD',
-				],
-			},
-	},
-	{
-		id: 2,
-		title: 'Food Street',
-		category: 'Mobile Application',
-		images: [FoodstreetImg1, FoodstreetImg2],
-		ProjectHeader: {
-			title: 'Front-End Develpement',
-			publishDate: 'Dec 3, 2021',
-			tags: 'UI / Frontend',
-		},
-		details: {
-			projectDescription: 'The Food Street App is a mobile application designed primarily for use in the food delivery industry. This application will allow customers to order food online from local restaurants. Once they’ve selected their desired food, the food will be delivered right to their doorstep! The application presents an interactive and up-to-date menu with all available options in an easy-to-use manner. The app has three user roles: the customer (the one who buys), the vendor (the one who produces), and the runner (the one who delivers). The app also has features such as push notifications, real-time tracking of orders, and payment integration.',			
-			features: [
-				'User profile',
-				'Order tracking options',
-				'List of recommendations',
-				'Ratings and reviews',
-				'Real-time GPS tracking'
-			]
-		},
-		technologies:
-			{
-				title: 'Tools & Technologies',
-				techs: [
-					'HTML',
-					'CSS',
-					'JavaScript',
-					'React Native',
-					'Styled Components',
-					'AdobeXD',
-				],
-			},
-	},
-	{
-		id: 3,
-		title: 'Memoree',
-		category: 'Mobile Application',
-		images: [MemoreeImg1, MemoreeImg2],
-		ProjectHeader: {
-			title: 'Front-End Develpement',
-			publishDate: 'Aug 26, 2020',
-			tags: 'UI / Frontend',
-		},
-		details: {
-			projectDescription: 'Memoree is an app that allows you to create collections of images and organize them. You can add images to the app and create collections of images. The app also allows you to organize your images by creating folders and subfolders. You can also add tags to your images to make them easier to find later on.',			
-			features: [
-				'Create collections of images',
-				'Organize images by creating folders and subfolders',
-				'Add tags to images to make them easier to find later on',
-			]
-		},
-		technologies:
-			{
-				title: 'Tools & Technologies',
-				techs: [
-					'HTML',
-					'CSS',
-					'JavaScript',
-					'React Native',
-					'Styled Components',
-					'AdobeXD',
-				],
-			},
-	},
-	{
-		id: 4,
-		title: 'Security Eye',
-		category: 'Mobile Application',
-		images: [SecurityEye1, SecurityEye2],
-		ProjectHeader: {
-			title: 'Front-End Develpement',
-			publishDate: 'Mar 26, 2023',
-			tags: 'UI / Frontend',
-		},
-		details: {
-			projectDescription: 'Security Eye is an app that allows you to monitor different security cameras through RTSP video streaming. The app also sends alerts notifications when someone is detected in the camera. The app has multi-language features and supports multiple cameras. You can view live video feeds from your cameras and control them remotely. You can also set up motion detection zones and receive alerts when motion is detected in those zones. The app also allows you to record video from your cameras and save it to your device.',			
-			features: [
-				'Monitor different security cameras through RTSP video streaming',
-				'Get alerts notifications when someone is detected in the camera',
-				'Multi-language support',
-			]
-		},
-		technologies:
-			{
-				title: 'Tools & Technologies',
-				techs: [
-					'HTML',
-					'CSS',
-					'JavaScript',
-					'React Native',
-					'Styled Components',
-					'AdobeXD',
-				],
-			},
-	},
-	{
-		id: 5,
-		title: 'Social Justice',
-		category: 'Mobile Application',
-		images: [SocialJustice1, SocialJustice2],
-		ProjectHeader: {
-			title: 'Front-End Develpement',
-			publishDate: 'Mar 26, 2023',
-			tags: 'UI / Frontend',
-		},
-		details: {
-			projectDescription: 'Social Justice is amobile application designed to empower individuals and promote positive change within society. This app serves as a platform where users can submit actionable items related to social issues, allowing others to take action and raise awareness. With features such as push notifications, Google ads, and auto-sharing to Twitter, Social Justice aims to foster a vibrant community of activists working together to address various concerns.',			
-			features: [	
-				'Push Notifications',
-				'Google Ads Integration',
-				'Auto-Share to Twitter',
-			]
-		},
-		technologies:
-			{
-				title: 'Tools & Technologies',
-				techs: [
-					'HTML',
-					'CSS',
-					'JavaScript',
-					'React Native',
-					'Styled Components',
-					'AdobeXD',
-				],
-			},
-	},
-	{
-		id: 6,
-		title: 'Reap',
-		category: 'Mobile Application',
-		images: [Reap1, Reap2],
-		ProjectHeader: {
-			title: 'Front-End Develpement',
-			publishDate: 'Apr 10, 2023',
-			tags: 'UI / Frontend',
-		},
-		details: {
-			projectDescription: 'Social Justice is amobile application designed to empower individuals and promote positive change within society. This app serves as a platform where users can submit actionable items related to social issues, allowing others to take action and raise awareness. With features such as push notifications, Google ads, and auto-sharing to Twitter, Social Justice aims to foster a vibrant community of activists working together to address various concerns.',			
-			features: [	
-				'Push Notifications',
-				'Google Ads Integration',
-				'Auto-Share to Twitter',
-			]
-		},
-		technologies:
-			{
-				title: 'Tools & Technologies',
-				techs: [
-					'HTML',
-					'CSS',
-					'JavaScript',
-					'React Native',
-					'Styled Components',
-					'AdobeXD',
-				],
-			},
-	},
+  {
+    id: 1,
+    title: 'Discreedly Health Platform',
+    category: 'Web Application',
+    images: [DiscreedlyImg1, DiscreedlyImg2],
+    ProjectHeader: {
+      title: 'Front-End Develpement',
+      publishDate: 'Jul 26, 2020',
+      tags: 'UI / Frontend',
+    },
+    details: {
+      projectDescription:
+        'The Discreedly app is designed to help HCPs (Health care provider) and MSLs (Medical science liaisons) manage their daily tasks more efficiently. The app will provide a platform for HCPs and MSLs to communicate with each other, share information, and collaborate on projects. The app will also provide access to the latest medical research, clinical trials, and other relevant information.',
+      features: [
+        'Secure messaging platform for HCPs and MSLs to communicate with each other',
+        'Access to the latest medical research, clinical trials, and other relevant information',
+        'Customizable dashboard that allows users to tailor the app to their specific needs',
+        'Task management system that helps users manage their daily tasks more efficiently',
+      ],
+    },
+    technologies: {
+      title: 'Tools & Technologies',
+      techs: [
+        'HTML',
+        'CSS',
+        'JavaScript',
+        'React.js',
+        'TailwindCSS',
+        'AdobeXD',
+      ],
+    },
+  },
+  {
+    id: 2,
+    title: 'Food Street',
+    category: 'Mobile Application',
+    images: [FoodstreetImg1, FoodstreetImg2],
+    ProjectHeader: {
+      title: 'Front-End Develpement',
+      publishDate: 'Dec 3, 2021',
+      tags: 'UI / Frontend',
+    },
+    details: {
+      projectDescription:
+        'The Food Street App is a mobile application designed primarily for use in the food delivery industry. This application will allow customers to order food online from local restaurants. Once they’ve selected their desired food, the food will be delivered right to their doorstep! The application presents an interactive and up-to-date menu with all available options in an easy-to-use manner. The app has three user roles: the customer (the one who buys), the vendor (the one who produces), and the runner (the one who delivers). The app also has features such as push notifications, real-time tracking of orders, and payment integration.',
+      features: [
+        'User profile',
+        'Order tracking options',
+        'List of recommendations',
+        'Ratings and reviews',
+        'Real-time GPS tracking',
+      ],
+    },
+    technologies: {
+      title: 'Tools & Technologies',
+      techs: [
+        'HTML',
+        'CSS',
+        'JavaScript',
+        'React Native',
+        'Styled Components',
+        'AdobeXD',
+      ],
+    },
+  },
+  {
+    id: 3,
+    title: 'Memoree',
+    category: 'Mobile Application',
+    images: [MemoreeImg1, MemoreeImg2],
+    ProjectHeader: {
+      title: 'Front-End Develpement',
+      publishDate: 'Aug 26, 2020',
+      tags: 'UI / Frontend',
+    },
+    details: {
+      projectDescription:
+        'Memoree is an app that allows you to create collections of images and organize them. You can add images to the app and create collections of images. The app also allows you to organize your images by creating folders and subfolders. You can also add tags to your images to make them easier to find later on.',
+      features: [
+        'Create collections of images',
+        'Organize images by creating folders and subfolders',
+        'Add tags to images to make them easier to find later on',
+      ],
+    },
+    technologies: {
+      title: 'Tools & Technologies',
+      techs: [
+        'HTML',
+        'CSS',
+        'JavaScript',
+        'React Native',
+        'Styled Components',
+        'AdobeXD',
+      ],
+    },
+  },
+  {
+    id: 4,
+    title: 'Security Eye',
+    category: 'Mobile Application',
+    images: [SecurityEye1, SecurityEye2],
+    ProjectHeader: {
+      title: 'Front-End Develpement',
+      publishDate: 'Mar 26, 2023',
+      tags: 'UI / Frontend',
+    },
+    details: {
+      projectDescription:
+        'Security Eye is an app that allows you to monitor different security cameras through RTSP video streaming. The app also sends alerts notifications when someone is detected in the camera. The app has multi-language features and supports multiple cameras. You can view live video feeds from your cameras and control them remotely. You can also set up motion detection zones and receive alerts when motion is detected in those zones. The app also allows you to record video from your cameras and save it to your device.',
+      features: [
+        'Monitor different security cameras through RTSP video streaming',
+        'Get alerts notifications when someone is detected in the camera',
+        'Multi-language support',
+      ],
+    },
+    technologies: {
+      title: 'Tools & Technologies',
+      techs: [
+        'HTML',
+        'CSS',
+        'JavaScript',
+        'React Native',
+        'Styled Components',
+        'AdobeXD',
+      ],
+    },
+  },
+  {
+    id: 5,
+    title: 'Social Justice',
+    category: 'Mobile Application',
+    images: [SocialJustice1, SocialJustice2],
+    ProjectHeader: {
+      title: 'Front-End Develpement',
+      publishDate: 'Mar 26, 2023',
+      tags: 'UI / Frontend',
+    },
+    details: {
+      projectDescription:
+        'Social Justice is amobile application designed to empower individuals and promote positive change within society. This app serves as a platform where users can submit actionable items related to social issues, allowing others to take action and raise awareness. With features such as push notifications, Google ads, and auto-sharing to Twitter, Social Justice aims to foster a vibrant community of activists working together to address various concerns.',
+      features: [
+        'Push Notifications',
+        'Google Ads Integration',
+        'Auto-Share to Twitter',
+      ],
+    },
+    technologies: {
+      title: 'Tools & Technologies',
+      techs: [
+        'HTML',
+        'CSS',
+        'JavaScript',
+        'React Native',
+        'Styled Components',
+        'AdobeXD',
+      ],
+    },
+  },
+  {
+    id: 6,
+    title: 'Reap',
+    category: 'Mobile Application',
+    images: [Reap1, Reap2],
+    ProjectHeader: {
+      title: 'Front-End Develpement',
+      publishDate: 'Apr 10, 2023',
+      tags: 'UI / Frontend',
+    },
+    details: {
+      projectDescription:
+        'Reap App is a versatile and user-friendly mobile application designed to provide a secure and efficient solution for encrypting and decrypting sensitive text, such as crypto wallet keys or any confidential information. With its robust encryption algorithms, multi-language support, and customizable color themes, Reap App ensures the protection and privacy of your valuable data.',
+      features: [
+        'Text Encryption and Decryption',
+        'Multi-Language Support',
+        'Customizable Color Themes',
+        'User-Friendly Interface',
+        'Data Security',
+      ],
+    },
+    technologies: {
+      title: 'Tools & Technologies',
+      techs: [
+        'HTML',
+        'CSS',
+        'JavaScript',
+        'React Native',
+        'Styled Components',
+        'AdobeXD',
+      ],
+    },
+  },
 ];
