f93c3164a6cb0175fbe6e9ff400066a8495aa742
ramizqazi <ramizqazi03@gmail.com>
2023-08-27 15:56:50 +0500

    change expereice

--- src/components/about/AboutCounter.jsx
+++ src/components/about/AboutCounter.jsx
@@ -1,42 +1,42 @@
-"use client"
-import { useCountUp } from 'react-countup';
+'use client';
+import {useCountUp} from 'react-countup';
 import CounterItem from './CounterItem';
 
 const AboutCounter = () => {
-	useCountUp({ ref: 'experienceCounter', end: 2, duration: 2 });
-	useCountUp({ ref: 'totalprojects', end: 50, duration: 2 });
-	useCountUp({ ref: 'feedbackCounter', end: 80, duration: 2 });
-	useCountUp({ ref: 'projectsCounter', end: 95, duration: 2 });
+  useCountUp({ref: 'experienceCounter', end: 3, duration: 2});
+  useCountUp({ref: 'totalprojects', end: 50, duration: 2});
+  useCountUp({ref: 'feedbackCounter', end: 80, duration: 2});
+  useCountUp({ref: 'projectsCounter', end: 95, duration: 2});
 
-	return (
-		<div className="mt-10 sm:mt-20 bg-primary-light dark:bg-ternary-dark shadow-sm">
-			<div className="font-general-medium container mx-auto py-20 block sm:flex sm:justify-between items-center">
-				<CounterItem
-					title="Years of experience"
-					counter={<span id="experienceCounter" />}
-					measurement=""
-				/>
+  return (
+    <div className="mt-10 sm:mt-20 bg-primary-light dark:bg-ternary-dark shadow-sm">
+      <div className="font-general-medium container mx-auto py-20 block sm:flex sm:justify-between items-center">
+        <CounterItem
+          title="Years of experience"
+          counter={<span id="experienceCounter" />}
+          measurement=""
+        />
 
-				<CounterItem
-					title="Total projects"
-					counter={<span id="totalprojects" />}
-					measurement=""
-				/>
+        <CounterItem
+          title="Total projects"
+          counter={<span id="totalprojects" />}
+          measurement=""
+        />
 
-				<CounterItem
-					title="Positive feedback"
-					counter={<span id="feedbackCounter" />}
-					measurement="%"
-				/>
+        <CounterItem
+          title="Positive feedback"
+          counter={<span id="feedbackCounter" />}
+          measurement="%"
+        />
 
-				<CounterItem
-					title="Projects completed"
-					counter={<span id="projectsCounter" />}
-					measurement="%"
-				/>
-			</div>
-		</div>
-	);
+        <CounterItem
+          title="Projects completed"
+          counter={<span id="projectsCounter" />}
+          measurement="%"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default AboutCounter;
