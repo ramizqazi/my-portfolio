83f1140c3ae98cdde86ccb0b925bb20fb1a70408
Ramiz Qazi <77078731+ramizqazi@users.noreply.github.com>
2023-11-29 14:21:56 +0500

    Update AppBanner.jsx

--- src/components/shared/AppBanner.jsx
+++ src/components/shared/AppBanner.jsx
@@ -44,28 +44,6 @@ const AppBanner = () => {
 				>
 					A Font-End Developer & Design Enthusiast
 				</motion.p>
-				<motion.div
-					initial={{ opacity: 0 }}
-					animate={{ opacity: 1 }}
-					transition={{
-						ease: 'easeInOut',
-						duration: 0.9,
-						delay: 0.3,
-					}}
-					className="flex justify-center sm:block"
-				>
-					<a
-						href="https://drive.google.com/file/d/1GKLNndQI9OH3NcMbRyHGffoFJb7TgHlM/view"
-						target='blank'
-						className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
-						aria-label="Download Resume"
-					>
-						<FiArrowDownCircle className="mr-2 sm:mr-3 h-5 w-5 sn:w-6 sm:h-6 duration-100"></FiArrowDownCircle>
-						<span className="text-sm sm:text-lg font-general-medium duration-100">
-							Download CV
-						</span>
-					</a>
-				</motion.div>
 			</div>
 			<motion.div
 				initial={{ opacity: 0, y: -180 }}
