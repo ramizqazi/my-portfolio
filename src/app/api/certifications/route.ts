bc27b24b5b1be40dad042660cce15dbc10ae30cc
ramizqazi <ramizqazi03@gmail.com>
2024-06-19 19:22:46 +0500

    fix: remove apis

--- src/app/api/certifications/route.ts
+++
@@ -1,8 +0,0 @@
-import {NextResponse} from 'next/server';
-import {client} from '../../../../sanity/lib/client';
-
-export const GET = async () => {
-  const payload = await client.fetch(`*[_type=="certification"]`);
-
-  return NextResponse.json(payload);
-};
--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -1,13 +1,8 @@
+import { client } from "../../../sanity/lib/client";
 import ProjectsGrid from "./ProjectsGrid";
 
-const getProjects = async () => {
-  const response = await fetch('https://my-portfolio-4xiyo1ux4-ramizqazis-projects.vercel.app/api/projects', { method: 'GET' });
-
-  return response.json();
-};
-
 const Projects = async () => {
-  const projects = await getProjects();
+  const projects = await client.fetch(`*[_type=="project"]`);
 
   return <ProjectsGrid projects={projects} />;
 };
